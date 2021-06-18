ad55aa2258b3140bebe5ab54b056eb9eeae2278a
shenting <1980970807@qq.com>
2021-06-18 18:01:02 +0800

    进度条拖动

--- video.js
+++ video.js
@@ -5,6 +5,7 @@ var playNode = document.querySelector(".playNode"),
     allTime = document.querySelector(".all"), /*视频长度*/
     load_line = document.querySelector('.load_line'), /*进度条*/
     ctrlNode = document.querySelector(".ctrlNode"), /*进度条上的拖动按钮*/
+    loadNode = document.querySelector('.loadNode'), /*进度条外层*/
     playBtn = true;
 
 // 播放暂停
@@ -50,10 +51,10 @@ videoNode.addEventListener("timeupdate", function(){
     //播放进度条：长度是当前播放时间占总时间的比例
     load_line.style.width = videoNode.currentTime/videoNode.duration*100 + "%";
     //进度条上的拖动按钮：和进度条位置一致
-    if(load_line.offsetWidth - 16 <= 0){
+    if(load_line.offsetWidth - 12 <= 0){
         ctrlNode.style.left = 0 + "px";
     }else{
-        ctrlNode.style.left = load_line.offsetWidth - 16 + 'px';
+        ctrlNode.style.left = load_line.offsetWidth - 12 + 'px';
     }
     //当前时间
     var nowSeconds = parseInt(videoNode.currentTime);
@@ -64,16 +65,35 @@ videoNode.addEventListener("timeupdate", function(){
 
 //拖拽进度条按钮
 ctrlNode.onmousedown = function(e){
-    var ev = e;
+    var ev = e || window.event;
     var l = ev.clientX - this.offsetLeft;
-    
+    videoNode.pause();
+    //拖动必须写在mousedown里
     document.onmousemove = function(e){
-        var ev = e;
-        var needX = ev.clientX -l;
+        var ev = e || window.event;
+        var needX = ev.clientX - l;
         console.log(needX);
+        if( needX < 0){
+            needX = 0;
+        }else if(needX > loadNode.offsetWidth - 12){
+            needX = loadNode.offsetWidth - 12;
+        }
         ctrlNode.style.left = needX + 'px';
+        load_line.style.width = (ctrlNode.offsetLeft + 12 ) / loadNode.offsetWidth *100 + '%';
     };
     document.onmouseup = function(){
+        //鼠标抬起后清空鼠标事件
         document.onmousemove =  document.onmouseup = null;
+        //鼠标抬起后视频正常播放
+        videoNode.play();
+        //判断拖动前是在播放状态还是在暂停状态
+        if(playBtn == false){
+            playNode.className = "pauseNode";
+            videoNode.play();
+        }
+        else{
+            playNode.className = "playNode";
+            videoNode.pause();
+        }
     };
 };
